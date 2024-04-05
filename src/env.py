2795eb1351411a3acd187366cdbf1525e8e6184c
Semenov <Semenov@mail.ru>
2024-04-05 20:45:23 +0400

    вынос в env размер игрового поля

--- src/env.py
+++ src/env.py
@@ -1,2 +1,16 @@
 def levelSize():
     return 3
+
+def sizeX():
+    if levelSize() == 3:
+        return 30
+        pass
+    if levelSize() == 2:
+        return 15
+    
+def sizeY():
+    if levelSize() == 3:
+        return 15
+        pass
+    if levelSize() == 2:
+        return 15
