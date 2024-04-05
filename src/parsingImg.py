aa2199069a2de9af01bbcc5d97f96369472b91d3
Semenov <Semenov@mail.ru>
2024-04-05 20:38:59 +0400

    логика разных уровней сложности сапера и проверка каждой пискеля а не 1 на определения значения ячейки

--- src/parsingImg.py
+++ src/parsingImg.py
@@ -1,7 +1,10 @@
 from PIL import Image
-
 import numpy
 import sys
+import collections
+
+import env
+
 numpy.set_printoptions(threshold=sys.maxsize)
 
 def _saveImgStatus():
@@ -12,12 +15,19 @@ def _saveImgStatus():
 
 def _parsingScreen():
     image = Image.open("img/monitor-1.png")
-    image_pole = image.crop((1015, 200, 1550, 810))
+    position = None
+    if env.levelSize() == 1:
+        # position = (1015, 200, 1550, 810)
+        pass
+        
+    if env.levelSize() == 2:
+        position = (1015, 200, 1550, 810)
+        
+    if env.levelSize() == 3:
+        position = (765, 200, 1795, 810)
+    
+    image_pole = image.crop(position)
     image_pole.save('img/pole.png', quality=95)
- 
-#  0,0,255 это 1
-
-
 
 def _checkStatusGames():
     image = Image.open('img/status.png').convert('RGB')
@@ -31,25 +41,37 @@ def _checkStatusGames():
 
 def parsingCell(y, x):
     image_pole = Image.open('img/pole.png').convert('RGB')
-    W_START = 7 + 32 * x
-    H_START = 79 + 32 * y
-    W_END = W_START + 32
-    H_END = H_START + 32
+    if env.levelSize() == 2:
+        W_START = 17 + 32 * x
+        H_START = 80 + 32 * y
+        W_END = W_START + 28
+        H_END = H_START + 28
+    if env.levelSize() == 3:
+        W_START = 20 + 32 * x
+        H_START = 80 + 32 * y
+        W_END = W_START + 32
+        H_END = H_START + 32
     image_cell = image_pole.crop((W_START, H_START, W_END, H_END))
     image_cell.save('img/cell.png', quality = 95)
     
 def cellPixelCheck():
     image_cell = Image.open('img/cell.png').convert('RGB')
-    width, height = image_cell.size
     pixel_values = list(image_cell.getdata())
-    pixel_values = numpy.array(pixel_values).reshape((width, height, 3))
-    # print(pixel_values[10][17])
-    if pixel_values[10][17][0] == 0 and pixel_values[10][17][1] == 0 and pixel_values[10][17][2] == 255:
-        return 1
-    if pixel_values[10][17][0] == 47 and pixel_values[10][17][1] == 139 and pixel_values[10][17][2] == 47:
-        return 2
-    if pixel_values[10][17][0] == 0 and pixel_values[10][17][1] == 0 and pixel_values[10][17][2] == 123:
-        return 3
+    # Цвета значении полей
+    returnList1 = list((0,0,225))
+    returnList2 = list((47,139,47))
+    returnList3 = list((0,0,123))
+    returnList4 = list((47, 47, 139))
+
+    for i in pixel_values:
+        if collections.Counter(i) == collections.Counter(returnList1):
+            return 1
+        if collections.Counter(i) == collections.Counter(returnList2):
+            return 2
+        if collections.Counter(i) == collections.Counter(returnList3):
+            return 3
+        if collections.Counter(i) == collections.Counter(returnList4):
+            return 4
     
 def checkStatus():
     _parsingScreen()
