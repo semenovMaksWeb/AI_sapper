c1c3b704992fd1470f2d196d4832a1b2bf4ccf45
Semenov <Semenov@mail.ru>
2024-04-05 20:39:04 +0400

    add

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-c:/Users/Maks/Desktop/II/caper/env/Scripts/python.exe c:/Users/Maks/Desktop/II/caper/src/main.py
+& c:/Users/Maks/Desktop/my/II/caper/env/Scripts/python.exe c:/Users/Maks/Desktop/my/II/caper/src/main.py
